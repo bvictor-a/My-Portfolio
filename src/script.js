95691d38e0cf9b689919f8f518a51cb523306e61
Bruno Victor <brunovictor.md@gmail.com>
2024-06-29 21:38:47 -0300

    Project

---
+++ src/script.js
@@ -0,0 +1,16 @@
+
+/* 
+const  header = document.getElementsByTagName('header')
+const sections = document.getElementsByTagName('section')
+
+console.log(sections);
+console.log(header);
+
+window.scroll(addEventListener('scroll', ()=>{
+    const scroollPosition = window.scrollTo() - header.clientHeight;
+
+    let activeSectionIndex = 0;
+
+    
+}))
+ */
