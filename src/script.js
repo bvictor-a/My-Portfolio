0f860dcd0fcadb3931fdb4d62a0379414d341aaa
Bruno Victor <brunovictor.md@gmail.com>
2024-12-06 22:18:39 -0300

    Project_v2

--- src/script.js
+++ src/script.js
@@ -14,3 +14,10 @@ window.scroll(addEventListener('scroll', ()=>{
     
 }))
  */
+
+$(document).ready(function () {
+    $('#mobile_btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active')
+        $('#mobile_btn').find('i').toggleClass('fa-x')
+    });
+})
